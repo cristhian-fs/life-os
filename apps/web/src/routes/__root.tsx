8895cb505665649eee2560440eb886dc3481e785
cris <cristhianfernandolp@gmail.com>
2026-08-26 09:36:55 -0300

    chore(web): wrap root file to QueryProvider

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,10 +2,13 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import '../styles.css'
 import { TooltipProvider } from '#/components/ui/tooltip'
 import { Toaster } from '#/components/ui/toast'
+import { ThemeProvider } from '#/components/theme-provider'
+import { queryClient } from '#/lib/react-query'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -13,22 +16,24 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <>
-      <TooltipProvider>
-        <Outlet />
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'TanStack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
-        <Toaster />
-      </TooltipProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="lifeos-theme">
+        <TooltipProvider>
+          <Outlet />
+          <TanStackDevtools
+            config={{
+              position: 'bottom-right',
+            }}
+            plugins={[
+              {
+                name: 'TanStack Router',
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+            ]}
+          />
+          <Toaster />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
